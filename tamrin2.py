22acdc373e1037ab2c369b73ed457cbfbd07e7d8
MatinSaghi <108825693+MatinSaghi@users.noreply.github.com>
2026-06-21 03:59:03 -0700

    Add files via upload

---
+++ tamrin2.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import commands
+
+intents=discord.Intents.default()
+intents.message_content=True
+intents.members=True
+intents.presences=True
+
+Bot= commands.Bot (command_prefix="+" , intents=intents)
+
+@Bot.event
+async def on_member_join(member):
+    log_channel = Bot.get_channel(1518192110770196630)     # چنل لاگ
+    welcome_channel = Bot.get_channel(1518198846436413573) # چنل ولکام
+
+    # لاگ ساده
+    if log_channel:
+        await log_channel.send(f"{member.name} join shod")
+
+    # امبد خوشگل
+    if welcome_channel:
+        embed = discord.Embed(
+            title="Welcome To Aqua server",
+            description=f"salam {member.mention} Khosh Omadi.",
+            color=discord.Color.green()
+        )
+
+        embed.set_thumbnail(url=member.avatar.url)  # عکس پروفایل
+        embed.set_footer(text=f"ID: {member.id}")
+
+        await welcome_channel.send(embed=embed)
+
+@Bot.event
+async def on_member_remove(member):
+    channel =  Bot.get_channel(1518192110770196630)
+    if channel:
+        await channel.send(f"{member.mention}left the server")  
+
+@Bot.event
+async def on_member_ban(guild , user):
+  channel = Bot.get_channel(1518197129657716777)
+  if channel:
+    await channel.send(f"{user.name}ban shod")
+
+@Bot.event
+async def on_member_unban(guild , user):
+  channel = Bot.get_channel(1518197129657716777)
+  if channel:
+    await channel.send(f"{user.name}unban shod")
